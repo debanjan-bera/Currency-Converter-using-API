63ea2695a8831639c8a8f998b45450c224454854
debanjan-bera <debanjan.bera25discord@gmail.com>
2024-11-04 00:08:08 +0530

    code is more readable

--- countrylist.js
+++ countrylist.js
@@ -1,182 +1,6 @@
-const currencyCountryCodes = {
-  ADA: "ADA",
-  AED: "AE",
-  AFN: "AF",
-  ALL: "AL",
-  AMD: "AM",
-  ANG: "AW",
-  AOA: "AO",
-  ARB: "ARB",
-  ARS: "AR",
-  AUD: "AU",
-  AWG: "AW",
-  AZN: "AZ",
-  BAM: "BA",
-  BBD: "BB",
-  BDT: "BD",
-  BGN: "BG",
-  BHD: "BH",
-  BIF: "BI",
-  BMD: "BM",
-  BNB: "BN",
-  BND: "BN",
-  BOB: "BO",
-  BRL: "BR",
-  BSD: "BS",
-  BTC: "BTC",
-  BTN: "BT",
-  BWP: "BW",
-  BYN: "BY",
-  BZD: "BZ",
-  CAD: "CA",
-  CHF: "CH",
-  CLF: "CL",
-  CNY: "CN",
-  COP: "CO",
-  CRC: "CR",
-  CUC: "CU",
-  CVE: "CV",
-  CZK: "CZ",
-  DAI: "DAI",
-  DJF: "DJ",
-  DKK: "DK",
-  DOP: "DO",
-  DOT: "DOT",
-  DZD: "DZ",
-  EGP: "EG",
-  ERN: "ER",
-  ETB: "ET",
-  ETH: "ETH",
-  EUR: "EU",
-  FJD: "FJ",
-  FKP: "FK",
-  GBP: "GB",
-  GEL: "GE",
-  GGP: "GG",
-  GHS: "GH",
-  GIP: "GI",
-  GMD: "GM",
-  GNF: "GN",
-  GTQ: "GT",
-  GYD: "GY",
-  HKD: "HK",
-  HNL: "HN",
-  HRK: "HR",
-  HTG: "HT",
-  HUF: "HU",
-  IDR: "ID",
-  ILS: "IL",
-  IMP: "IM",
-  INR: "IN",
-  IQD: "IQ",
-  IRR: "IR",
-  ISK: "IS",
-  JEP: "JE",
-  JMD: "JM",
-  JOD: "JO",
-  JPY: "JP",
-  KES: "KE",
-  KGS: "KG",
-  KHR: "KH",
-  KMF: "KM",
-  KPW: "KP",
-  KRW: "KR",
-  KWD: "KW",
-  KYD: "KY",
-  KZT: "KZ",
-  LAK: "LA",
-  LBP: "LB",
-  LKR: "LK",
-  LRD: "LR",
-  LSL: "LS",
-  LTC: "LTC",
-  LTL: "LT",
-  LVL: "LV",
-  LYD: "LY",
-  MAD: "MA",
-  MDL: "MD",
-  MGA: "MG",
-  MKD: "MK",
-  MMK: "MM",
-  MNT: "MN",
-  MOP: "MO",
-  MRO: "MR",
-  MUR: "MU",
-  MVR: "MV",
-  MWK: "MW",
-  MXN: "MX",
-  MYR: "MY",
-  MZN: "MZ",
-  NAD: "NA",
-  NGN: "NG",
-  NIO: "NI",
-  NOK: "NO",
-  NPR: "NP",
-  NZD: "NZ",
-  OMR: "OM",
-  OP: "OP",
-  PAB: "PA",
-  PEN: "PE",
-  PGK: "PG",
-  PHP: "PH",
-  PKR: "PK",
-  PLN: "PL",
-  PYG: "PY",
-  QAR: "QA",
-  RON: "RO",
-  RSD: "RS",
-  RUB: "RU",
-  RWF: "RW",
-  SAR: "SA",
-  SBD: "SB",
-  SCR: "SC",
-  SDG: "SD",
-  SEK: "SE",
-  SGD: "SG",
-  SHP: "SH",
-  SLL: "SL",
-  SOL: "SV",
-  SOS: "SO",
-  SRD: "SR",
-  STD: "ST",
-  SVC: "SV",
-  SYP: "SY",
-  SZL: "SZ",
-  THB: "TH",
-  TJS: "TJ",
-  TMT: "TM",
-  TND: "TN",
-  TOP: "TO",
-  TRY: "TR",
-  TTD: "TT",
-  TWD: "TW",
-  TZS: "TZ",
-  UAH: "UA",
-  UGX: "UG",
-  USD: "US",
-  UYU: "UY",
-  UZS: "UZ",
-  VEF: "VE",
-  VND: "VN",
-  VUV: "VU",
-  WST: "WS",
-  XAF: "XAF",
-  XAG: "XAG",
-  XAU: "XAU",
-  XCD: "XCD",
-  XDR: "XDR",
-  XPD: "XPD",
-  XPT: "XPT",
-  XRP: "XRP",
-  YER: "YE",
-  ZAR: "ZA",
-  ZMK: "ZM",
-  ZWL: "ZW",
-};
 const amountData = document.querySelector("input");
 amountData.value = 1;
 const resultOfConversion = document.querySelector(".res");
-
 
 const insertOptionData = (select1,select2,data) => {
   const dataCn = Object.keys(currencyCountryCodes);
@@ -187,12 +11,14 @@ const insertOptionData = (select1,select2,data) => {
       select2.insertAdjacentHTML("beforeend", markup);
   }
 };
-const selectFlag = (dropDownList,data,select,natIcon) =>{
+const selectFlag = (dropDownList,data,select,natIcon,isSelect) =>{
   dropDownList.addEventListener("change",()=>{
       select = dropDownList.value;
       console.log(select);
       const cnIcon = data[select].icon
       natIcon.innerHTML =`<img src="${cnIcon}" class="img">`
+      if (isSelect) selected1stValue = select;
+      else selected2ndValue = select;
   })
 }
 const CurrConvert = async(from,to) =>{
@@ -203,13 +29,23 @@ const CurrConvert = async(from,to) =>{
   const convertedAmount = (dataOfCurrency.result).toFixed(0);
   resultOfConversion.innerHTML = `<h1>${amountData.value} ${from} = ${convertedAmount} ${to}</h1>`
 }
-function selecteCountryDetails(list,isSelect){
-  list.addEventListener('change',()=>{
-      const selectedValue = list.value;
-      if (isSelect) selected1stValue = selectedValue;
-      else selected2ndValue = selectedValue;
-  })
-}
+// // // const displayErrorMessage = (message) => {
+// //   const errorContainer = document.getElementById("errorContainer");
+// //   errorContainer.textContent = message;
+// //   errorContainer.style.display = "block"; // Show the error message
+// // // };
+
+
+
+
+
+// function selecteCountryDetails(list,isSelect){
+//   list.addEventListener('change',()=>{
+//       const selectedValue = list.value;
+//       if (isSelect) selected1stValue = selectedValue;
+//       else selected2ndValue = selectedValue;
+//   })
+// }
 // let fromSelectNat = 'USD';
 // let toSelectedCountry = 'INR';
 // const conversionBtn = document.querySelector(".btn")
@@ -248,14 +84,10 @@ function selecteCountryDetails(list,isSelect){
 //        else toSelectedCountry = currencyCode;
 //     }
 //     triggerConversion();
-    
 //   })
-
 // }
 
-
 // console.log(fromSelectNat,toSelectedCountry);
-
 // export function triggerConversion() {
 //   const amount = inputBox.value === ''? 1:inputBox.value
 //   if (fromSelectNat && toSelectedCountry) {
--- script2.js
+++ script2.js
@@ -4,7 +4,6 @@ firstCountryIcon = document.querySelector(".pic1"),
 secondCountryIcon = document.querySelector(".pic2"),
 conversionBtn = document.querySelector(".btn");
 
-
 let selected1stValue = 'USD'
 let selected2ndValue ='INR'
 const countryCode = async () => {
@@ -12,25 +11,24 @@ try{
     const fetchCnData = await fetch("https://api.currencyfreaks.com/v2.0/supported-currencies");
     const data = await fetchCnData.json();
     populateDropdowns(data.supportedCurrenciesMap)
+    conversionBtn.addEventListener('click',()=>{
+        CurrConvert(selected1stValue,selected2ndValue)
+    });
 }
 catch(error){
     console.error("Error fetching currency data:", error);
     // displayErrorMessage("Failed to load currency data. Please try again later.");
 }
 }
-countryCode()
-
-// CurrConvert(selected1stValue,selected2ndValue)
 const populateDropdowns = (data) => {
     insertOptionData(selectFirstList,selectSecondList,data)
-    selectFlag(selectFirstList,data,selected1stValue,firstCountryIcon)
-    selectFlag(selectSecondList,data,selected2ndValue,secondCountryIcon)
-    selecteCountryDetails(selectFirstList,true)
-    selecteCountryDetails(selectSecondList, false)
+    selectFlag(selectFirstList,data,selected1stValue,firstCountryIcon,true);
+    selectFlag(selectSecondList,data,selected2ndValue,secondCountryIcon,false)
 }
 
-conversionBtn.addEventListener('click',(ele)=>{
-    console.log(selected1stValue,selected2ndValue);
-    CurrConvert(selected1stValue,selected2ndValue)
-    console.log(selected1stValue,selected2ndValue);
-});
+CurrConvert(selected1stValue,selected2ndValue)
+countryCode()
+
+const fetchCurr = async()=>{
+    const url = await fetch()
+}
