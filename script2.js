84c6a737a7c8c241147334ded75314426bfe05ae
debanjan-bera <debanjan.bera25discord@gmail.com>
2024-11-05 12:47:09 +0530

    I used import and export module

--- script2.js
+++ script2.js
@@ -1,11 +1,9 @@
+import{insertOptionData,selectFlag,CurrConvert,selected1stValue,selected2ndValue, amountData} from "./function.js"
 const selectFirstList = document.getElementById("nation1"),
 selectSecondList = document.getElementById("nation2"),
 firstCountryIcon = document.querySelector(".pic1"),
 secondCountryIcon = document.querySelector(".pic2"),
 conversionBtn = document.querySelector(".btn");
-
-let selected1stValue = 'USD'
-let selected2ndValue ='INR'
 const countryCode = async () => {
 try{
     const fetchCnData = await fetch("https://api.currencyfreaks.com/v2.0/supported-currencies");
@@ -26,18 +24,7 @@ const populateDropdowns = (data) => {
     selectFlag(selectFirstList,data,selected1stValue,firstCountryIcon,true);
     selectFlag(selectSecondList,data,selected2ndValue,secondCountryIcon,false)
 }
-
+amountData.value = 1;
 CurrConvert(selected1stValue,selected2ndValue)
 countryCode()
 
-const fetchCurr = async()=>{
-try{
-    const url = await fetch("https://api.fxratesapi.com/currencies")
-    const data3 = await url.json()
-    console.log(data3.INR.symbol);
-}
-catch(error){
-    console.log('error:',error);
-}
-}
-fetchCurr()
